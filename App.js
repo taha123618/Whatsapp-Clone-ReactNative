050300941f8a5f91af61fcef52e42846ba755609
Taha Ahmed 123618 <tahaahmedanees2@gmail.com>
2022-01-17 03:31:04 +0500

    third change

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import {useEffect , useState} from 'react'
 import { View, 
 Text,
 StatusBar,
@@ -5,21 +6,69 @@ StyleSheet } from 'react-native'
 import Login from './source/Screen/Login'
 import SignUp from './source/Screen/SignUp'
 import Home from './source/Screen/Home'
+import Chat from './source/Screen/Chat'
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import auth from '@react-native-firebase/auth'
+import {MaterialIcons} from 'react-native-vector-icons/MaterialIcons'
 
 // naviagation
 
 const Stack = createStackNavigator();
 
 const Naviagation = ()=> {
+  const [user,setUser ] = useState()
+ /*/ compontent mounth hone ke bad sirf  1 he bar chale 
+  take listner ko register krwao 1 bar /*/
+  useEffect(() => {
+  const unregister = auth().onAuthStateChanged(userExist=>{
+      if(userExist) setUser('userExist')
+      else setUser('')
+    })
+    // Run on app exit 
+    return()=>{
+      unregister()
+    }
+
+  }, [])
+
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator 
+      screenOptions={{
+        headerTintColor:'green'
+      }}>
+
+      {
+        user?
+        <>
+        <Stack.Screen name="home" component={Home} 
+          options={{
+            headerRight:()=><MaterialIcons
+            name='account-circle'
+            color='green'
+            style={{marginRight:10}}
+            onPress={()=>auth().signOut()}
+             />,
+             title:"WhatsApp"
+          }}
+        >
+          {/* use over coustom props  */}
+          {props => <Home {...props} user={user} />}
+        </Stack.Screen> 
+        <Stack.Screen name="chat" 
+        options={({ route }) => ({ title : route.params.name})} 
+        >
+        {props => <Chat {...props} user={user} />}
+        </Stack.Screen>
+        </>
+        :
+        <>
         <Stack.Screen name="login" component={Login} options={{headerShown:false}} />
         <Stack.Screen name="signup" component={SignUp} options={{headerShown:false}} />
+        </>
+      }
       </Stack.Navigator>
     </NavigationContainer>
   );
